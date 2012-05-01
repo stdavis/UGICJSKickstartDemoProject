580c1f426395adac7e62c134cf2ba5c8288b4b66
Scott Davis <stdavis@utah.gov>
2012-05-01 16:35:29 -0600

    Started coding session #3

---
+++ js/core.js
@@ -0,0 +1,13 @@
+console.log('hello');
+// comments provide helpful information to make your code easier
+// for others (including your future self) to understand
+
+function myFunction() {
+	console.log('here1');
+}
+
+var car = {
+	color: 'blue',
+	numDoors: 4,
+	paidFor: true
+};
