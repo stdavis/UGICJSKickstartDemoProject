fe55a241f2498504baeaed84928003d22ce84fdd
Scott Davis <stdavis@utah.gov>
2013-04-01 22:41:08 -0600

    Finished up coding session #5 and archived results. Added url object and var plus some comments.

--- js/core.js
+++ js/core.js
@@ -1,5 +1,16 @@
 console.log('hello');
+// comments are lines of text that are not executed
+
+var baseUrl = 'http://mapserv.utah.gov/arcgis/rest/services/';
+
+var urls = {
+    hro2012: baseUrl + 'AerialPhotography_Color/HRO2012Color6Inch_4Band/ImageServer',
+    hro2009: baseUrl + 'AerialPhotography_Color/HRO2009_Color1Foot/ImageServer',
+    uao2003: baseUrl + 'AerialPhotography_Color/UAO2003_Color1Foot/ImageServer',
+    doq1990: baseUrl + 'AerialPhotography_BlackWhite/DOQ1990s_1Meter/ImageServer'
+};
 
 function init() {
+    // sets up the app
     console.log('init fired');
 }
